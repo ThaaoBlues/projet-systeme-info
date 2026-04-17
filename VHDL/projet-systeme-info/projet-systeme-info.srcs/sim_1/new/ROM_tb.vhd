ae129e57a4469b3dd53241111a05bbf8e87dbf06
mougnibas <mougnibas@insa-toulouse.fr>
2026-04-17 15:10:01 +0200

    MEMOIRES

--- VHDL/projet-systeme-info/projet-systeme-info.srcs/sim_1/new/ROM_tb.vhd
+++ VHDL/projet-systeme-info/projet-systeme-info.srcs/sim_1/new/ROM_tb.vhd
@@ -1,43 +1,78 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/17/2026 03:04:47 PM
--- Design Name: 
--- Module Name: ROM_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity ROM_tb is
---  Port ( );
+-- Entité vide pour un testbench
 end ROM_tb;
 
 architecture Behavioral of ROM_tb is
 
+    -- Déclaration du composant à tester
+    component ROM
+        PORT (
+            ADDR   : in std_logic_vector(7 downto 0);
+            CLK    : in std_logic;
+            SORTIE : out std_logic_vector(31 downto 0)
+        );
+    end component;
+
+    -- Signaux de pilotage
+    signal ADDR   : std_logic_vector(7 downto 0) := (others => '0');
+    signal CLK    : std_logic := '0';
+    signal SORTIE : std_logic_vector(31 downto 0);
+
+    -- Paramètres de temps
+    constant CLK_PERIOD : time := 10 ns;
+
 begin
 
+    -- Instanciation de la ROM
+    uut: ROM 
+        PORT MAP (
+            ADDR   => ADDR,
+            CLK    => CLK,
+            SORTIE => SORTIE
+        );
+
+    -- Générateur d'horloge
+    clk_process : process
+    begin
+        while now < 200 ns loop
+            CLK <= '0';
+            wait for CLK_PERIOD/2;
+            CLK <= '1';
+            wait for CLK_PERIOD/2;
+        end loop;
+        wait;
+    end process;
+
+    -- Processus de stimulus
+    stim_proc: process
+    begin		
+        -- Attente de la stabilisation
+        wait for 20 ns;
+
+        -- Lecture de l'adresse 0
+        ADDR <= std_logic_vector(to_unsigned(0, 8));
+        wait for CLK_PERIOD;
+
+        -- Lecture de l'adresse 1
+        ADDR <= std_logic_vector(to_unsigned(1, 8));
+        wait for CLK_PERIOD;
+
+        -- Lecture de l'adresse 42
+        ADDR <= std_logic_vector(to_unsigned(42, 8));
+        wait for CLK_PERIOD;
+
+        -- Parcours de quelques adresses dans une boucle
+        for i in 10 to 15 loop
+            ADDR <= std_logic_vector(to_unsigned(i, 8));
+            wait for CLK_PERIOD;
+        end loop;
+
+        report "Fin de la simulation de lecture ROM";
+        wait;
+    end process;
 
 end Behavioral;
